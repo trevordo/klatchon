9c2857e10ed97fc9c61a8be362eb3bb22c15bea3
Trevor Do <trevordo@gmail.com>
2015-12-01 21:13:00 +0000

    Changes from Screen Builder

--- Pleniere/components/profileView/index.js
+++ Pleniere/components/profileView/index.js
@@ -1,67 +1,10 @@
-/**
- * profile view model
- */
-'use strict'
+'use strict';
 
-app.profileView = (function () {
-
-    var profileModel = kendo.observable({
-        firstname: "",
-        lastname: "",
-        Displayname: "",
-
-    });
-
-    var details = {
-        onShow: function (e) {
-            
-            var everliveApiKey = "koWUnpcxRvIqHF7T";
-				var everliveScheme = 'https';
-
-            var el = new Everlive({
-					apiKey: everliveApiKey,
-					scheme: everliveScheme
-				});
-            
-            el.Users.currentUser()
-                .then(function (data) {
-                        alert(JSON.stringify(data));
-                    },
-                    function (error) {
-                        alert(JSON.stringify(error));
-                    });
-
-            var dataProvider,
-                typeName,
-                userId,
-                vm;
-
-
-            dataProvider = app.data.defaultProvider;
-            typeName = "Users";
-            userId =  "";
-                
-            dataProvider.data(typeName).getById(userId, function (data) {
-                var resultItem = data.result;
-                profileModel.set("firstname", resultItem.firstname);
-                profileModel.set("lastname", resultItem.lastname);
-                profileModel.set("DisplayName", resultItem.DisplayName);
-               
-            }, function (err) {
-                alert("There was an error" + err.message);
-            });
-
-        }
-
-    };
-
-    return {
-        profileModel: profileModel,
-        details: details
-    };
-})();
+app.profileView = kendo.observable({
+    onShow: function() {},
+    afterShow: function() {}
+});
 
 // START_CUSTOM_CODE_profileView
-
 
 // END_CUSTOM_CODE_profileView
